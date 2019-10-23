739017a8f4fd38549aba6e464a6bae7dc067a0d6
Pankaj Kumar <pankajthekush@gmail.com>
2019-10-23 21:06:05 +0530

    Added flaskk gui

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask , render_template
-
+from flask import Flask , render_template, request
+import  os
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/success',methods=['POST'])
+def success():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save( os.path.join(app.config['UPLOAD_FOLDER'],  file.filename))
+        return render_template('success.html')
 
 
 if __name__ == "__main__":
